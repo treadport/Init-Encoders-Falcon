83be47efbdf2dd377c7873641077307b39ac245b
treadport <147753424+treadport@users.noreply.github.com>
2024-05-23 14:34:46 -0600

    Add files via upload

---
+++ Init Encoders Falcon/Init Encoders Falcon/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using System;
+using System.Threading;
+using CTRE.Phoenix.MotorControl;
+using CTRE.Phoenix.MotorControl.CAN;
+
+namespace Init_Encoders_Falcon
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            TalonFX _talon = new TalonFX(11); //CHANGE: this is the motor's ID number
+
+            _talon.ConfigSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor,
+                                                0,
+                                                30);
+
+            /* loop forever */
+            while (true)
+            {
+                
+                /* wait a bit */
+                System.Threading.Thread.Sleep(100);
+            }
+        }
+    }
+}
